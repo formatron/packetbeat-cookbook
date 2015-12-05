27b36aa41c691cadf99e677bcb302e9c034f9002
Peter Halliday <pghalliday@gmail.com>
2015-12-05 10:11:59 +0100

    add option to disable packetbeat

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
 default['formatron_packetbeat']['version'] = nil
 
+default['formatron_packetbeat']['enabled'] = true
+
 default['formatron_packetbeat']['logstash']['host'] = 'localhost'
 default['formatron_packetbeat']['logstash']['port'] = 5044
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,7 @@
 version = node['formatron_packetbeat']['version']
 
+enabled = node['formatron_packetbeat']['enabled']
+
 host = node['formatron_packetbeat']['logstash']['host']
 port = node['formatron_packetbeat']['logstash']['port']
 
@@ -15,10 +17,11 @@ template '/etc/packetbeat/packetbeat.yml' do
     host: host,
     port: port
   )
-  notifies :restart, 'service[packetbeat]', :delayed
+  notifies :restart, 'service[packetbeat]', :delayed if enabled
 end
 
 service 'packetbeat' do
   supports status: true, restart: true, reload: false
-  action [ :enable, :start ]
+  action [ :enable, :start ] if enabled
+  action [ :disable, :stop ] unless enabled
 end
